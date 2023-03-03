71db1b6b242a9c781eb5802b106c1f17cd704a5c
Amelissarc <melissa.rinconft@gmail.com>
2023-03-03 19:49:44 -0300

    twitterFollowCard

--- 00-project/src/App.jsx
+++ 00-project/src/App.jsx
@@ -1,13 +1,29 @@
 import './App.css';
-import { TwitterFollowCard } from './TwitterFollorCard.jsx';
+import { TwitterFollowCard } from './TwitterFollowCard.jsx';
 
 export function App () {
+  const formatUserName = (userName) => `@${userName}`;  
+
     return (
         <section className='App'>
-          <TwitterFollowCard isFollowing userName="Reddit" name="Reddit Argentina"/>
-          <TwitterFollowCard isFollowing={false} userName="pheralb" name="Pablo Hernandez"/>
-          <TwitterFollowCard isFollowing userName="Amelissarc" name="Melissa Rincón"/>
-          <TwitterFollowCard isFollowing userName="jablarias" name="Jaime Blanco"/>
+          <TwitterFollowCard 
+            formatUserName={formatUserName} 
+            userName="RappiArgentina" 
+            initialIsFollowing={true}
+            name="Rappi Argentina"/>
+          <TwitterFollowCard 
+          formatUserName={formatUserName} 
+            userName="Emova_arg" 
+            name="Emova - Concesionario del Subte de Buenos Aires"/>
+          <TwitterFollowCard 
+          formatUserName={formatUserName}  
+            userName="adidasAR" 
+            initialIsFollowing={true}
+            name="adidas Argentina"/>
+          <TwitterFollowCard 
+          formatUserName={formatUserName}  
+            userName="spotify" 
+            name="Spotify"/>
         </section>      
     )
 }
